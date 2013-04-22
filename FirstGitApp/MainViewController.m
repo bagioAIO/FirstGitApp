ff78094782872f45786329d68fe909fd6cdd47b9
Eric Vargas <eric.vargas.mx@gmail.com>
2013-04-22 17:10:06 -0500

    Se corrigio un error de que no mostraba la imagen en el scroll view

--- FirstGitApp/MainViewController.m
+++ FirstGitApp/MainViewController.m
@@ -19,13 +19,13 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+    UIImage *image = [UIImage imageNamed:@"mario_wallpaper.jpg"];
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
+    
+    [self.scrollView addSubview:imageView];
+    self.scrollView.contentSize = image.size;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
